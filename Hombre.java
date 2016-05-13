e78d7225522a98c33b6659be7aa9cd63e6fd16e4
ulisesten <ulises.mtz.el@gmail.com>
2016-05-13 18:39:14 -0500

    Se añadieron pasos a jugador y las clases Menu y Boton

--- Hombre.java
+++ Hombre.java
@@ -9,11 +9,17 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Hombre extends Jugador
 {
     private int pasos=0;
+    private int pausa=5;
     
     private GreenfootImage ImgDer;
     private GreenfootImage ImgIzq;
     private GreenfootImage ImgUp;
     private GreenfootImage ImgDown;
+    
+    private GreenfootImage ImgDer2;
+    private GreenfootImage ImgIzq2;
+    private GreenfootImage ImgUp2;
+    private GreenfootImage ImgDown2;
     
     public Hombre()
     {
@@ -22,6 +28,11 @@ public class Hombre extends Jugador
         ImgIzq= new GreenfootImage("MaleLeft1.png");
         ImgUp= new GreenfootImage("MaleUp1.png");
         ImgDown= new GreenfootImage("MaleDown1.png");
+        
+        ImgDer2= new GreenfootImage("MaleRight2.png");
+        ImgIzq2= new GreenfootImage("MaleLeft2.png");
+        ImgUp2= new GreenfootImage("MaleUp2.png");
+        ImgDown2= new GreenfootImage("MaleDown2.png");
     }
     
     public void act()
@@ -51,28 +62,36 @@ public class Hombre extends Jugador
     public void mueveIzq(int x,int y)
     {
         setLocation(x-1,y);
-        wait(5);
+        wait(pausa);
         move(-10);
         setImage(ImgIzq);
+        wait(pausa);
+        setImage(ImgIzq2);
     }
     
     public void mueveDer(int x,int y)
     {
         setLocation(x+1,y);
-        wait(5);
+        wait(pausa);
         move(10);
         setImage(ImgDer);
+        wait(pausa);
+        setImage(ImgDer2);
     }
     
     public void mueveUp(int x,int y){
         setLocation(x,y-10);
-            wait(5);
-            setImage(ImgUp);
+        wait(pausa);
+        setImage(ImgUp);
+        wait(pausa);
+        setImage(ImgUp2);
     }
     
     public void mueveDown(int x,int y){
         setLocation(x,y+10);
-            wait(5);
-            setImage(ImgDown);
+        wait(pausa);
+        setImage(ImgDown);
+        wait(pausa);
+        setImage(ImgDown2);
     }
 }
