9c43cce1fcf385051302b31af3d172480bc77cef
Ulises <ulises.mtz.el@gmail.com>
2016-05-14 18:11:55 -0500

    Actualización de Menu

---
+++ BotonOpciones.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class BotonOpciones here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BotonOpciones extends Actor
+{
+    /**
+     * Act - do whatever the BotonOpciones wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
--- Menu.java
+++ Menu.java
@@ -9,16 +9,16 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Menu extends World
 {
 
-    /**
-     * Constructor for objects of class Menu.
-     * 
-     */
+    private int x=100;// Controlando posición x de botones en conjunto
+    private int y=2;// Controlando posición y de botones en conjunto
     public Menu()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        // Agregando y posicionando botones
         super(600, 400, 1);
-        addObject(new BotonJugar(),400,250);
-        addObject(new BotonSalir(),180,320);
-        addObject(new BotonAyuda(),180,180);
+        addObject(new BotonOpciones(),x+201,y+59);
+        addObject(new BotonSalir(),x+60,y+195);
+        addObject(new BotonAyuda(),x+60,y+333);
+        addObject(new BotonJugar(),x+270,y+267);
     }
 }
